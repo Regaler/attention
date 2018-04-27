afcfb5c6419f6efed67f7dcebe0a54bc9ec3932e
Regaler <akalsdnr@kaist.ac.kr>
2018-04-27 12:28:09 +0900

    <Changed to python 2>

--- SE.py
+++ SE.py
@@ -65,7 +65,7 @@ def train(epoch):
         loss.backward()
         optimizer.step()
         if batch_idx % 10 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data), len(train_loader.dataset), 100. * batch_idx / len(train_loader), loss.data[0]))
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data), len(train_loader.dataset), 100. * batch_idx / len(train_loader), loss.item()))
             log_value('loss', loss, 391*(epoch-1) + batch_idx)
 
     if epoch % 20 == 0:
